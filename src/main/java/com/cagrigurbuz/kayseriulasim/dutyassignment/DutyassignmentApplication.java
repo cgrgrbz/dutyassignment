9683469157fb64059259433e8102cc828607e76c
cgrgrbz <cgrgrbz@msn.com>
2020-08-25 13:48:55 +0300

    Initial commit

---
+++ src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/DutyassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.cagrigurbuz.kayseriulasim.dutyassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DutyassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DutyassignmentApplication.class, args);
+	}
+
+}
