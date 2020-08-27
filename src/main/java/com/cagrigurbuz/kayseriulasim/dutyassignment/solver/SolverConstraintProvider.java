420e0960de14a3a0765cac2905e7b89896940690
cgrgrbz <cgrgrbz@msn.com>
2020-08-27 19:21:30 +0300

    add NoOverlappingDuties constraint
    No overlapping duties assigned to a employee.

--- src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/solver/SolverConstraintProvider.java
+++ src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/solver/SolverConstraintProvider.java
@@ -1,10 +1,16 @@
 package com.cagrigurbuz.kayseriulasim.dutyassignment.solver;
 
+import java.util.Objects;
+
 import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
 import org.optaplanner.core.api.score.stream.uni.UniConstraintStream;
+import static org.optaplanner.core.api.score.stream.Joiners.equal;
+import static org.optaplanner.core.api.score.stream.Joiners.greaterThan;
+import static org.optaplanner.core.api.score.stream.Joiners.lessThan;
+
 import com.cagrigurbuz.kayseriulasim.dutyassignment.domain.Duty;
 
 public class SolverConstraintProvider implements ConstraintProvider {
@@ -14,6 +20,7 @@ public class SolverConstraintProvider implements ConstraintProvider {
         return new Constraint[] {
         		requiredRegionOfEmployee(constraintFactory),
         		assignEveryDuty(constraintFactory),
+        		noOverlappingDuties(constraintFactory),
         };
 	}
 	
@@ -39,6 +46,17 @@ public class SolverConstraintProvider implements ConstraintProvider {
         return constraintFactory.fromUnfiltered(Duty.class)
                 .filter(duty -> duty.getEmployee() == null)
                 .penalize("Assign every duty.", HardSoftScore.ONE_SOFT);
+    }
+    
+    Constraint noOverlappingDuties(ConstraintFactory constraintFactory) {
+        return getAssignedDutyConstraintStream(constraintFactory)
+                .join(Duty.class,
+                        equal(Duty::getEmployee),
+                        lessThan(Duty::getStartDateTime, Duty::getEndDateTime),
+                        greaterThan(Duty::getEndDateTime, Duty::getStartDateTime))
+                .filter((duty, otherDuty) -> !Objects.equals(duty, otherDuty))
+                .penalize("No Overlapping Duties.", HardSoftScore.ofHard(100));
+                //.penalizeConfigurableLong("No Overlapping Duties.", (duty, otherDuty) -> otherDuty.dutyLengthInMinutes());
     }
     
 }
