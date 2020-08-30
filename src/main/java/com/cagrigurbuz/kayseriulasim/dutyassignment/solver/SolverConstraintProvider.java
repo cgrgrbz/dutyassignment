c8294e1e3a6efcf582559e18dae911eb1193a312
cgrgrbz <cgrgrbz@msn.com>
2020-08-30 23:29:50 +0300

    Added sameDutyOnWeekDays constraint

--- src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/solver/SolverConstraintProvider.java
+++ src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/solver/SolverConstraintProvider.java
@@ -1,5 +1,6 @@
 package com.cagrigurbuz.kayseriulasim.dutyassignment.solver;
 
+import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
@@ -7,10 +8,12 @@ import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
 import org.optaplanner.core.api.score.stream.Constraint;
 import org.optaplanner.core.api.score.stream.ConstraintFactory;
 import org.optaplanner.core.api.score.stream.ConstraintProvider;
+import org.optaplanner.core.api.score.stream.Joiners;
 import org.optaplanner.core.api.score.stream.uni.UniConstraintStream;
 import static org.optaplanner.core.api.score.stream.Joiners.equal;
 import static org.optaplanner.core.api.score.stream.Joiners.greaterThan;
 import static org.optaplanner.core.api.score.stream.Joiners.lessThan;
+import static org.optaplanner.core.api.score.stream.ConstraintCollectors.count;
 
 import com.cagrigurbuz.kayseriulasim.dutyassignment.domain.Duty;
 
@@ -23,6 +26,8 @@ public class SolverConstraintProvider implements ConstraintProvider {
         		assignEveryDuty(constraintFactory),
         		noOverlappingDuties(constraintFactory),
         		breakBetweenTwoConsecutiveDutyAtLeast12Hours(constraintFactory),
+        		getSameDutiesForTheSameWeek(constraintFactory),
+        		maxSixWorkingDayInAWeek(constraintFactory),
         };
 	}
 	    
@@ -43,7 +48,7 @@ public class SolverConstraintProvider implements ConstraintProvider {
     Constraint assignEveryDuty(ConstraintFactory constraintFactory) {
         return constraintFactory.fromUnfiltered(Duty.class)
                 .filter(duty -> duty.getEmployee() == null)
-                .penalize("Assign every duty.", HardSoftScore.ofSoft(10 ));
+                .penalize("Assign every duty.", HardSoftScore.ofSoft(10));
     }
     
     //No overlapping duties for a employee
@@ -68,11 +73,20 @@ public class SolverConstraintProvider implements ConstraintProvider {
                 .penalize("At least 12 Hours break after the Duty.", HardSoftScore.ONE_SOFT);
     }
     
-	//TODO
-    
-    //Assign same duty on weekdays
+	//TODO CHECK IT IF CORRECT    
+    //Assign same duty to the same employee on weekdays
+    Constraint getSameDutiesForTheSameWeek(ConstraintFactory constraintFactory) {
+        return constraintFactory.fromUnfiltered(Duty.class)
+                .join(Duty.class,
+                        Joiners.equal(Duty::getName),
+                        Joiners.equal(Duty::getDutyWeekOfYear),
+                        Joiners.lessThan(Duty::getId),
+                        Joiners.greaterThan(Duty::getId))
+                .filter((d1, d2) -> !Objects.equals(d1.getEmployee(), d2.getEmployee()))
+                .penalize("same duty for the weekday duties", HardSoftScore.ONE_HARD);
+    }
     
     //Maximum 6 days working for each employee
-    
+
     //Same dutyType after each duty
 }
