7ae45d40c202b7d18f1024d490f2750b03483fe4
cgrgrbz <cgrgrbz@msn.com>
2020-09-05 03:16:34 +0300

    Update domain importers accordingly

--- src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/utils/DutyListXLSXFileIO.java
+++ src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/utils/DutyListXLSXFileIO.java
@@ -47,7 +47,9 @@ public class DutyListXLSXFileIO {
 
 				String dutyName, employeeCode, employeeName, dutyRegion, dutyType, dutyLoad, dutyStartDate, dutyStartTime,
 						dutyEndDate, dutyEndTime;
-
+				
+				int dutyPriority, maxMonthlyWorkingHour;
+				
 				LocalDate startDate, endDate;
 				LocalTime startTime, endTime;
 
@@ -59,7 +61,8 @@ public class DutyListXLSXFileIO {
 				dutyStartTime = formatter.formatCellValue(row.getCell(9));
 				dutyEndDate = formatter.formatCellValue(row.getCell(10));
 				dutyEndTime = formatter.formatCellValue(row.getCell(11));
-
+				dutyPriority = (int) row.getCell(12).getNumericCellValue();
+				
 				startDate = LocalDate.parse(dutyStartDate);
 				startTime = LocalTime.parse(dutyStartTime);
 				endDate = LocalDate.parse(dutyEndDate);
@@ -69,6 +72,7 @@ public class DutyListXLSXFileIO {
 				duty.setRegion(dutyRegion);
 				duty.setType(dutyType);
 				duty.setLoad(Double.parseDouble(dutyLoad));
+				duty.setPriority(dutyPriority);
 				duty.setStartDateTime(LocalDateTime.of(startDate, startTime));
 				duty.setEndDateTime(LocalDateTime.of(endDate, endTime));
 
@@ -77,21 +81,23 @@ public class DutyListXLSXFileIO {
 				if (row.getCell(2) != null) {
 					employeeCode = row.getCell(2).getStringCellValue();
 					employeeName = row.getCell(3).getStringCellValue();
+					maxMonthlyWorkingHour = (int) row.getCell(13).getNumericCellValue();
 										
 					Employee employee = employeeService.getEmployeeByCode(employeeCode);
 					
-					if (employee == null) {
-						employee = new Employee(null, employeeCode, employeeName, dutyRegion);
-						employeeService.addEmployee(employee);
-					}
+					//if (employee == null) {
+					//	employee = new Employee(null, employeeCode, employeeName, dutyRegion, maxMonthlyWorkingHour);
+					//	employeeService.addEmployee(employee);
+					//}
 					
 					duty.setEmployee(employee);
 				}
 				
-				boolean isCurrentDuty;
-				isCurrentDuty = (duty.getEmployee() == null) ? true : false;
+//				boolean isCurrentDuty;
+//				isCurrentDuty = (duty.getEmployee() == null) ? true : false;
+//				duty.setItCurrentDutyToBeAssigned(isCurrentDuty);
 				
-				duty.setItCurrentDutyToBeAssigned(isCurrentDuty);
+				duty.setItCurrentDutyToBeAssigned(false);
 							
 				toSave.add(duty);
 			}
