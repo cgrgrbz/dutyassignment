45414e43adef896c18e81613c5fe7a4c4e2fc5f5
cgrgrbz <cgrgrbz@msn.com>
2020-09-05 03:15:03 +0300

    Update SolverController.java

--- src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/controller/SolverController.java
+++ src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/controller/SolverController.java
@@ -1,15 +1,10 @@
 package com.cagrigurbuz.kayseriulasim.dutyassignment.controller;
 
 import java.time.LocalDate;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-
 import org.optaplanner.core.api.score.ScoreManager;
-import org.optaplanner.core.api.solver.SolverJob;
 import org.optaplanner.core.api.solver.SolverManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,9 +22,11 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/solver")
 @Api(tags = "Solver")
 public class SolverController {
-
+	
+	public static final Long scheduleId = 1L;
+	
 	@Autowired
-	private SolverManager<Schedule, UUID> solverManager;
+	private SolverManager<Schedule, Long> solverManager;
 
 	@Autowired
 	private ScoreManager<Schedule> scoreManager;
@@ -43,48 +40,38 @@ public class SolverController {
 	@Autowired
 	private ScheduleService scheduleService;
 
-	UUID problemId = UUID.randomUUID();
-
-	Schedule problem = new Schedule();
-
-	Schedule solution = null;
-
 	@ApiOperation("Solve for schedule")
 	@PostMapping("/solve")
-	public Schedule solve(@RequestParam("scheduleStartDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate scheduleStartDate) {
+	public void solve(@RequestParam("scheduleStartDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate scheduleStartDate) {
 		
-		problem.setScheduleStartDate(scheduleStartDate);
-
-		problem.setDutyList(dutyService.getDutyList());
-		problem.setEmployeeList(employeeService.getEmployeeList());
+		Schedule schedule = getSchedule(scheduleId);
 		
-		//TODO
-		//Because we getting the date from post request, we want the solver solve only the current duties equal or grater then the given date
-		//I flagging the duties manually with looping all the in memory duties, but will add a parameterization stuff for the schedule so we can filter accordingly
-		problem.setCurrentDutyFlag(scheduleStartDate);		
-		dutyService.updateDutyList(problem.getDutyList());
+		schedule.setScheduleStartDate(scheduleStartDate);
+		schedule.setCurrentDutyFlag(scheduleStartDate);
+		dutyService.updateDutyList(schedule.getDutyList());
 		
-		SolverJob<Schedule, UUID> solverJob = solverManager.solve(problemId, problem);
+		saveSchedule(schedule);
+		
+		solverManager.solveAndListen(scheduleId, this::getSchedule, this::saveSchedule);
+		
+	}
+	
+	
+	public Schedule getSchedule(Long id) {
+		
+      Schedule schedule = new Schedule(scheduleId, employeeService.getEmployeeList(), dutyService.getDutyList());
+      //scheduleService.addSchedule(schedule);
 
-		try {
-			// Wait until the solving ends
-			solution = solverJob.getFinalBestSolution();
-			scoreManager.updateScore(solution);
-		} catch (InterruptedException | ExecutionException e) {
-			throw new IllegalStateException("Solving failed.", e);
-		}
-
+      return schedule;		
+	}
+	
+	public void saveSchedule(Schedule solution) {
 		dutyService.updateDutyList(solution.getDutyList());
-		
-		//delete the previous schedule.. then save the new one.
-		scheduleService.clearSchedule();
-		
-		return scheduleService.addSchedule(solution);
 	}
 
-	@ApiOperation("Explain the solution")
-	@GetMapping("/explain")
-	public String explain() {
-		return scoreManager.explainScore(solution);
-	}
+//	@ApiOperation("Explain the solution")
+//	@GetMapping("/explain")
+//	public String explain() {
+//		return scoreManager.explainScore(solution);
+//	}
 }
