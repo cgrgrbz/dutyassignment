d6e71f0e38a4e22e6c51ae40441f29ea4d26ec2d
cgrgrbz <cgrgrbz@msn.com>
2020-09-05 03:12:49 +0300

    Update DutySelectionFilter.java

--- src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/domain/DutySelectionFilter.java
+++ src/main/java/com/cagrigurbuz/kayseriulasim/dutyassignment/domain/DutySelectionFilter.java
@@ -8,9 +8,6 @@ public class DutySelectionFilter implements PinningFilter<Schedule, Duty>{
 
 	@Override
 	public boolean accept(Schedule solution, Duty entity) {
-		LocalDate scheduleStartDate = solution.getScheduleStartDate();
-		LocalDate dutyStartDate = entity.getStartDateTime().toLocalDate();
-		return dutyStartDate.isBefore(scheduleStartDate); //accept only the entities that be considered in the current schedule, comparing the scheduleStartDate
-		//////////?? how
+		return !entity.isItCurrentDutyToBeAssigned();
 	}
 }
